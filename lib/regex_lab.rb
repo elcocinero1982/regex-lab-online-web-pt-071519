5354ddca061408da1dfad1a8695c4e23148c4a25
jose luis <cortezluisjose@gmail.com>
2019-08-04 01:40:09 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,7 @@ text.match
 end
 
 def words_five_letters_long(text)
-
+text.match
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
@@ -16,5 +16,5 @@ return first_word_capitalized_and_ends_with_punctuation
 end
 
 def valid_phone_number?(phone)
-phone.scan jkhhfv
+phone.scan 
 end
